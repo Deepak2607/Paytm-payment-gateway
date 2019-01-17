f61ba000effb96a8b397b71ee0a7af403fa63268
Deepak Kumrawat <deepakkumrawat8@gmail.com>
2019-01-17 20:09:27 +0530

    ''

--- routes/admin/testtxn.js
+++ routes/admin/testtxn.js
@@ -31,7 +31,7 @@ res.render('index.ejs');
             }
         }
         console.log(paramarray);
-        paramarray['CALLBACK_URL'] = 'http://localhost:3000/response';  // in case if you want to send callback
+        paramarray['CALLBACK_URL'] = 'https://paytm-api-2607.herokuapp.com/response';  // in case if you want to send callback
         console.log(PAYTM_MERCHANT_KEY);
         checksum.genchecksum(paramarray, PAYTM_MERCHANT_KEY, function (err, result) 
         {
